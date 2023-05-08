337b1c68935797af311006e3c50c811395099509
Dondo Dano <oshawott.jr@gmail.com>
2023-05-08 16:51:37 +0800

    change daisyui to flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,13 @@ module.exports = {
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js"
     ],
     theme: {
         extend: {},
     },
-    plugins: [require("daisyui")],
-    daisyui: {
-        themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
-    },
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
 
